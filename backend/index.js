d4421bbbb54e2115a10847626f3ab80a829e5817
Elyes Bali <baliaynos@gmail.com>
2025-11-22 14:09:06 +0100

    new push

--- backend/index.js
+++ backend/index.js
@@ -257,9 +257,10 @@ app.post("/improve-cv",verifyToken, // Get req.userId
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("/.*/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
+  app.get(/.*/, (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+
 }
 
 // ----------------- Study Session: START -----------------
